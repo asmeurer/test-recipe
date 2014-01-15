1421b6398f4a2d636d881d9e6708f8ea13a4a381
Aaron Meurer <asmeurer@gmail.com>
2014-01-15 17:32:52 -0600

    Use different names for the Python scripts

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 mkdir -p $PREFIX/bin
 cp $RECIPE_DIR/non-python.sh $PREFIX/bin/
 cp $RECIPE_DIR/binary $PREFIX/bin/
-cp $RECIPE_DIR/python $PREFIX/bin/
+cp $RECIPE_DIR/pythonscript $PREFIX/bin/
 cp $RECIPE_DIR/python.py $PREFIX/bin/
